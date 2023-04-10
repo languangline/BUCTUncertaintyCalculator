bc594c33eaaabf89b93de1e9e5770b643d5bc0d6
Languang <Languangline@gmail.com>
2023-04-10 16:33:16 +0800

    require less args now

--- main.py
+++ main.py
@@ -16,13 +16,13 @@ def nFliter(n):
   
 def main():
     # data
-    n = 5
     sum = 0
-    ins = 0.02
-    x = np.array([99.58, 99.62, 99.60, 99.58, 99.62])
+    ins = 0.01
+    x = np.array([13.10, 12.88, 13.10, 13.09, 13.12])
     m_x = np.mean(x)
 
 
+    n = x.size
     for num in np.nditer(x):
         sum += pow((num - m_x), 2)
     tp = [1.84, 1.32, 1.20, 1.14, 1.11, 1.09, 1.08, 1.07, 1.06, 1.05, 1.04, 1.03, 1.00]
@@ -30,7 +30,7 @@ def main():
     ub = ins / sqrt(3)
     u1theta = sqrt(pow(ua, 2) + pow(ub, 2))
     u2theta = 2 * u1theta
-    print(ua, ub, u1theta, u2theta)
+    print(m_x, ua, ub, u1theta, u2theta)
   
 if __name__ == "__main__":
     main()
